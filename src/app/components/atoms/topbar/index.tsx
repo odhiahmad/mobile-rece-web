b66bf4502405a42358477834ca818c011c05f12b
Yoga Darmawan <the.yogadarmawan@gmail.com>
2021-09-21 09:19:21 +0700

    feat: add go back to topbar

--- src/app/components/atoms/topbar/index.tsx
+++ src/app/components/atoms/topbar/index.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import Styles from './styles';
 import LeftChevron from 'assets/img/left-chevron.png';
+import { useHistory } from 'react-router';
 
 export function TopBar() {
   // const body = document.body;
   // body.style.backgroundColor = colors.black20;
+  const history = useHistory();
   return (
     <>
       <Styles>
-        <div className="topbar flex">
+        <div className="topbar flex" onClick={() => history.goBack()}>
           <img src={LeftChevron} alt="Left Chevron" />
         </div>
         <div className="topbar-filler"></div>
