29d8943cc72fbeaac5cf4ce6d67f6ec4d592e350
Odhi Ahmad Hidayat <odhiahmad15@gmail.com>
2021-10-08 11:31:52 +0700

    url api

--- src/services/index.ts
+++ src/services/index.ts
@@ -40,7 +40,7 @@ export function setUpAxios() {
 
 export default function Axios(config: AxiosRequestConfig) {
   // DEFINE BASE URL BASED ON SERVICE
-  const baseURL = 'https://d552-182-253-34-181.ngrok.io';
+  const baseURL = 'https://d5d3-182-253-34-181.ngrok.io';
   // INIT HEADER
   const headers = {
     ...initialHeader,
